e6acefd2ff92d402b457a33de9b9b0819d12a519
TheGlu3guy <lucca.anthoine@hotmail.com>
2021-01-02 20:42:15 +0100

    inscription

--- server/routes/api.js
+++ server/routes/api.js
@@ -16,8 +16,6 @@ client.connect()
 const users = []
 
 /* Route à faire :
-  - inscription --FAIT--
-  - connexion --FAIT--
   - ajout d'une annonce
   - recupération des annonces dispo (en fonction des dates etc... donc plusieurs routes)
   - récup annonce favoris
@@ -89,76 +87,56 @@ router.post('/register', async (req, res) => {
   })
   res.send('ok')
 })
-/*
-class Objet {
-  constructor () {
-    this.createdAt = new Date()
-    this.updatedAt = new Date()
-    this.array = []
-  }
-}
+router.post('/registerProducteur', async (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  const pseudo = req.body.pseudo
+  const description = req.body.descripton
 
-router.use((req, res, next) => {
-  // l'utilisateur n'est pas reconnu
-  if (typeof req.session.objet === 'undefined') {
-    req.session.objet = new Objet()
-  }
-  next()
-})
+  const result = await client.query({
+    text: 'SELECT * FROM users WHERE email=$1',
+    values: [email]
+  })
 
-
-router.get('/panier', (req, res) => {
-  res.json(req.session.panier)
-})
-
-router.post('/panier', (req, res) => {
-  req.body.articleId
-
-  if (isNaN(quantity)) {
-    res.status(400).json({ message: "You cannot add a quantity of 0 in your cart"})
+  if (result.rows.length > 0) {
+    res.status(401).json({
+      message: 'user already exists'
+    })
     return
   }
+  // si on a pas trouvé l'utilisateur
+  // alors on le crée
 
-  const newArticle = {
-    id: articleId,
-    quantity
+  const hash = await bcrypt.hash(password, 10)
+
+  await client.query({
+    text: `INSERT INTO users(email, password, pseudo)
+    VALUES ($1, $2, $3)
+    `,
+    values: [email, hash, pseudo]
+  })
+
+  const result2 = await client.query({
+    text: 'SELECT lastval()'
+  })
+
+  const idUser = result2.rows[0].lastval
+  if(typeof description === 'undefined'){
+    await client.query({
+      text: `INSERT INTO producteur(id_user)
+      VALUES ($1)
+      `,
+      values: [idUser]
+    })
+  }else {
+    await client.query({
+      text: `INSERT INTO producteur(id_user, description)
+      VALUES ($1, $2)
+      `,
+      values: [idUser, description]
+    })
   }
-
-  res.send(newArticle)
+  
+  res.send('ok')
 })
-
-router.put('/panier/:articleId', (req, res) => {
-  const articleId = parseInt(req.params.articleId)
-
-  if (isNaN(quantity) || quantity <= 0) {
-    res.status(400).json({ message: "You cannot add a quantity of 0 in your cart"})
-    return
-  }
-
-  res.send()
-})
-
-router.route('/article/:articleId')
-  .get(parseArticle, (req, res) => {
-  })
-  .put(parseArticle, (req, res) => {
-    const name = req.body.name
-    const description = req.body.description
-    const image = req.body.image
-    const price = parseInt(req.body.price)
-
-    req.article.name = name
-    req.article.description = description
-    req.article.image = image
-    req.article.price = price
-    res.send()
-  })
-
-  .delete(parseArticle, (req, res) => {
-    const index = articles.findIndex(a => a.id === req.articleId)
-
-    articles.splice(index, 1) // remove the article from the array
-    res.send()
-  })
-*/
 module.exports = router
