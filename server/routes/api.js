8ce5e68f2fac12a0db14faa04e50bf6aee303d5b
TheGlu3guy <lucca.anthoine@hotmail.com>
2021-01-02 13:56:16 +0100

    ajout connexion-inscription

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,94 @@
 const express = require('express')
 const router = express.Router()
 //const example = require('../data/example.js')
+const bcrypt = require('bcrypt')
+const { Client } = require('pg')
 
+const client = new Client({
+ user: 'postgres',
+ host: 'localhost',
+ password: '123',
+ database: 'transverse'
+})
+
+client.connect()
+
+const users = []
+
+/* Route à faire :
+  - inscription --FAIT, à tester--
+  - connexion --FAIT, à tester--
+  - ajout d'une annonce
+  - recupération des annonces dispo (en fonction des dates etc... donc plusieurs routes)
+  - récup annonce favoris
+  - ajout annonce favoris
+*/
+
+
+
+router.post('/login', async (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  const result = await client.query({
+    text: 'SELECT * FROM user WHERE email=$1',
+    values: [email]
+  })
+
+  if (result.rows.length === 0) {
+    res.status(401).json({
+      message: 'user doesnt exist'
+    })
+    return
+  }
+  // si on a pas trouvé l'utilisateur
+  // alors on le crée
+  const user = result.rows[0]
+
+  if (await bcrypt.compare(password, user.password)) {
+    // alors connecter l'utilisateur
+    req.session.userId = user.id
+    res.json({
+      id: user.id,
+      email: user.email
+    })
+  } else {
+    res.status(401).json({
+      message: 'bad password'
+    })
+    return
+  }
+})
+
+router.post('/register', async (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  const pseudo = req.body.pseudo
+
+  const result = await client.query({
+    text: 'SELECT * FROM users WHERE email=$1',
+    values: [email]
+  })
+
+  if (result.rows.length > 0) {
+    res.status(401).json({
+      message: 'user already exists'
+    })
+    return
+  }
+  // si on a pas trouvé l'utilisateur
+  // alors on le crée
+
+  const hash = await bcrypt.hash(password, 10)
+
+  await client.query({
+    text: `INSERT INTO users(email, password, pseudo)
+    VALUES ($1, $2, $3)
+    `,
+    values: [email, hash, pseudo]
+  })
+  res.send('ok')
+})
 /*
 class Objet {
   constructor () {
