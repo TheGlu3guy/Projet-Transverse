76d8c81ed4aa3199597a6a5a6213f639934f80ee
mathisbeau <mathis.beau@efrei.net>
2021-01-05 13:03:23 +0100

    mentions légales et notre histoire

--- client/components/MentionLegales.vue
+++ client/components/MentionLegales.vue
@@ -1,23 +1,40 @@
 <template>
     <div>
         <h1>Mentions légales</h1>
-        <h2>Mentions propres au site</h2>
-        <p>Hébergeur : PlanetHosteur<br>
-        <h2>Mentions relatives à l'utilisation des cookies</h2>
-        <h2>Mentions relatives à l'utilisation de données personnelles</h2>
-        <p>Les informations éventuellement receuillies auprès de vous 
-        lors de votre utilisation du site web sont strictement destinées
-        à l'exploitant en vue pour répondre aux obejectifs suivant : </p>
-        <ul>
-            <li>Création d'un profil (utilisateur et/ou vendeur)</li>
-            <li>Mise en place d'un formulaire de contact</li>
-            <li>Réception d'informations par mail</li>
-        </ul>        
-        <p>Ces informations ne seront en aucun cas transmises à des 
-        personnes tiers. Conformément aux dispositions de la loi 
-        « informatiques et libertés » loi n°78-17 du 6 janvier 1978 
-        modifiée, vous bénéficiez des droits d’accès, de rectification
-        et d’opposition à vos données personnelles.</p>
+        <div id="hébergeur">
+            <h2>Hébergement</h2>
+            <p>Hébergeur : PlanetHosteur<br>
+                Adresse : 4416 Louis B. Mayer, Laval (Québec) H7P 0G1, Canada<br>
+                Site de l'hébergeur : <a href="https://www.planethoster.com/fr/">https://www.planethoster.com/fr/</a><br>
+                Numéro de téléphone : FR (Nº Vert): 0 805 080 426
+        </div>
+        <div id="cookies">
+            <h2>Mentions relatives à l'utilisation des cookies</h2>
+            <p>Ce site n'utilise pas de cookies.</p>
+        </div>
+        <div id="données_personnelles">
+            <h2>Mentions relatives à l'utilisation de données personnelles</h2>
+            <p>Local&Frais recueille des renseignements fournis par écrit pour fournir et améliorer les services de ce site.
+                Les informations éventuellement receuillies auprès de vous 
+                lors de votre utilisation du site web sont strictement destinées
+                à l'exploitant pour répondre aux objectifs suivant : </p>
+            <ul>
+                <li>Création d'un profil (utilisateur et/ou vendeur)</li>
+                <li>Mise en place d'un formulaire de contact</li>
+                <li>Réception d'informations par mail</li>
+            </ul>        
+            <p>Ces informations ne seront en aucun cas transmises à des personnes tiers.<br> 
+                Conformément aux dispositions de la loi « informatiques et libertés » loi n°78-17 du 6 janvier 1978 modifiée, 
+                vous bénéficiez des droits d’accès, de rectification et d’opposition à vos données personnelles.</p>
+        </div>
+        <div id="CNIL">
+            <h2>Déclaration à la CNIL</h2>
+            <p>Les bases de données utilisées étant fictives, ce site n'a fait l'objet d'aucune déclaration à la CNIL.</p>
+        </div>
+        <div id="propriété_intellectuelle">
+            <h2>Propriété intellectuelle</h2>
+            <p>L'ensemble des éléments figurant sur le site demeure la propriété exclusive de Local&Frais et sont protégés par le droit de la proprité intellectuelle.</p>
+        </div>
     </div>
 </template>
 
@@ -25,4 +42,5 @@
 </style>
 
 <script>
+
 </script>
---
+++ client/components/Notre_histoire.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+        <h2>Notre histoire</h2>
+        <p>Ce site a été entièrement réalisé et imaginé par un groupe de 5 étudiants en école d'ingénieurs dans le cadre d'un projet de groupe semestriel ...</p>
+        <img src="img/logo_efrei.png">
+    </div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+</script>
