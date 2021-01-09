3bd58c0a0144eb604cf117862112c95572aa2b3b
mathisbeau <mathis.beau@efrei.net>
2021-01-09 22:49:54 +0100

    début profil producteur

---
+++ client/components/Mon_profil.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Mon profil</h1>
+        <div id="mes_produits">
+            <h2>Mes produits actuellement en vente</h2>
+        </div>
+        <div id="mes_labels">
+            <h2>Mes labels</h2>
+            <button>Ajouter des labels</button>
+        </div>
+        <div id="mes_points_de_vente">
+            <h2>Mes points de vente</h2>
+            <button>Ajouter un point de vente</button>
+        </div>
+        <div id="messages">
+            <h2>Mes messages</h2>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+</script>
